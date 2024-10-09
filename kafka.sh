3bc71cb5a4011cff82bf5773f007a68d5756a88c
HarishHacks <harish1729r@gmail.com>
2024-10-09 17:38:06 +0530

    Update kafka.sh

--- kafka.sh
+++ kafka.sh
@@ -8,4 +8,17 @@ echo
 echo "INSTALLING Kafka"
 wget https://downloads.apache.org/kafka/3.8.0/kafka-3.8.0-src.tgz
 tar -xzf kafka-3.8.0-src.tgz
+cd kafka-3.8.0-src/
+./gradlew jar -PscalaVersion=2.13.14
+echo
+echo
+echo "Starting zookeper"
+sleep 3
+bin/zookeeper-server-start.sh config/zookeeper.properties &
+echo "Starting server"
+sleep 3
+bin/kafka-server-start.sh config/server.properties &
+sleep 3
+echo "THANK YOU!"
+sleep 3
 echo "THANK YOU!"
