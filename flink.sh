f98d2a1888003c44969542c0f10c3eefc74e4a36
HarishHacks <harish1729r@gmail.com>
2024-10-09 17:01:42 +0530

    Update flink.sh

--- flink.sh
+++ flink.sh
@@ -11,14 +11,6 @@ then
 wget https://dlcdn.apache.org/flink/flink-1.19.1/flink-1.19.1-bin-scala_2.12.tgz
 tar -xzf flink-1.19.1-bin-scala_2.12.tgz
 fi
-echo
-echo "ENTER THE TEXT YOU WANT:"
-read text
-echo $text >input.txt
 cd flink-1.19.1/
-bin/stop-cluster.sh
 bin/start-cluster.sh
-bin/flink run examples/batch/WordCount.jar -input /home/ubuntu/input.txt -output /home/ubuntu/output.txt
-echo "WORD COUNT SAVED IN /home/ubuntu/output.txt"
-echo
 echo "THANK YOU!"
